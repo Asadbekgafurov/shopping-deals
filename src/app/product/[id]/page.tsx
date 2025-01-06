faca4186c202c7338fde30b00be1c1828088602a
AsadbekGofurov <114288439+Asadbekgafurov@users.noreply.github.com>
2025-01-06 21:52:09 +0500

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,48 +1,39 @@
-import { useParams } from 'next/navigation';
-import Image from 'next/image';
+"use client"
+import CustomImage from "@/components/image";
+import { notFound } from "next/navigation";
 
-interface Product {
-  id: string;
-  name: string;
-  description: string;
-  imageUrl: string;
-  price: number;
+interface Props {
+  params: {
+    id: string;
+  };
 }
 
-const fetchProduct = async (id: string): Promise<Product> => {
-  const res = await fetch(`https://api.example.com/products/${id}`, {
-    cache: 'no-store', // Dinamik yuklash uchun.
-  });
-
-  if (!res.ok) {
-    throw new Error('Mahsulot ma’lumotlari yuklanmadi.');
-  }
-
-  return res.json();
-};
-
-const ProductPage = async () => {
-  const params = useParams();
-  const productId = params?.id as string;
-
-  if (!productId) {
-    return <div>Mahsulot ID topilmadi.</div>;
-  }
-
+const ProductDetailedPage = async ({ params: { id } }: Props) => {
   try {
-    const product = await fetchProduct(productId);
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+    const product = await res.json();
 
     return (
-      <div>
-        <h1>{product.name}</h1>
-        <Image src={product.imageUrl} alt={product.name} width={500} height={300} />
-        <p>{product.description}</p>
-        <p>Narxi: ${product.price}</p>
+      <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-48 pb-10">
+        <CustomImage product={product} />
+
+        <div className="divide-2">
+          <div className="space-y-2 pb-8">
+            <h1 className="text-2xl md:text-4xl font-bold">{product.title}</h1>
+            <h2 className="text-gray-500 font-bold text-xl md:text-3xl">
+              ${product.price}
+            </h2>
+          </div>
+
+          <div>
+            <p className="text-xs md:text-sm">{product.description}</p>
+          </div>
+        </div>
       </div>
     );
   } catch (error) {
-    return <div>Mahsulotni yuklashda xatolik yuz berdi.</div>;
+    notFound();
   }
 };
 
-export default ProductPage;
+export default ProductDetailedPage;
